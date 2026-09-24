e5db168273cea99dce4c08d5f2b473702d0ab554
KG359 <kevin.reyes-garcia@aceamandla.org>
2026-09-24 16:07:09 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,11 +4,11 @@
    ========================================================= */
 
 const FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
+  apiKey: "AIzaSyCoH2E00z0ymcc27ooedRTjX1Yjco3LEVM4",
+  authDomain: "mikuchatv2.firebaseapp.com",
   databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  projectId: "mikuchatv2",
+  storageBucket: "mikuchatv2.firebasestorage.app",
+  messagingSenderId: "528225688056",
+  appId: "1:528225688056:web:f0eeeb9ee19d7943f99e0d"
 };
